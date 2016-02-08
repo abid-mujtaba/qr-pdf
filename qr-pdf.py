10ab5a2a744f2c200d163f055acc5a718ea12307
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2016-02-08 14:21:15 +0500

    Add skeletal template and associated processing

--- qr-pdf.py
+++ qr-pdf.py
@@ -4,8 +4,13 @@
 #
 # This script takes a String as input (on the command line) and creates a tex file which can be compiled using pdflatex to produce the corresponding QR Code.
 
+import jinja2
 import qrcode
 import sys
+
+BASE_FILE = "qr-pdf"
+TEMPLATE_FILE = BASE_FILE + ".template"
+TEX_FILE = BASE_FILE + ".tex"
 
 
 def main():
@@ -26,6 +31,17 @@ def main():
     length = len(matrix)        # The side length of the QR Code
 
     print("Length: " + str(length))
+
+    # Load the template
+    loader = jinja2.FileSystemLoader(searchpath=".")        # Search for templates in current folder
+    env = jinja2.Environment(loader=loader)
+    template = env.get_template(TEMPLATE_FILE)
+
+    # Write rendered template to tex file
+    fout = open(TEX_FILE, "w")
+    fout.write(template.render())
+    fout.close()
+
 
 
 if __name__ == '__main__':
